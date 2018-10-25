ace90ed5e98cc50bd4b87874f4d79232f8d009d9
咸狮子 <jigede@live.cn>
2018-10-25 18:22:49 +0800

    中文机翻

--- bot.py
+++ bot.py
@@ -2,18 +2,18 @@ from slackclient import SlackClient
 import re
 import time
 
-# instantiate Slack client
+# 实例化Slack客户端
 slack_client = SlackClient('你的Token') # 在Slack API页面内获取
 
-# constants
-RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+# 常量
+RTM_READ_DELAY = 1 # 从RTM读取之间延迟1秒
 EXANPLE_COMMAND = "我是不是很帅" # 这里就是命令关键词，bot从Slack RTM 接收信息，并匹配，匹配成功就执行相应的if。
-MENTION_REGEX = "(.*)"
+MENTION_REGEX = "(.*)" #这是匹配全部命令的意思，会回复所有从RTM收到的消息。
 def parse_bot_commands(slack_events):
     """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
+        解析来自Slack RTM API的事件列表，以查找bot命令。
+        如果找到bot命令，则此函数返回命令和通道的元组。
+        如果未找到，则此函数返回None，None。
     """
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
@@ -23,28 +23,26 @@ def parse_bot_commands(slack_events):
 
 def parse_direct_mention(message_text):
     """
-        Finds a direct mention (a mention that is at the beginning) in message text
-        and returns the user ID which was mentioned. If there is no direct mention, returns None
+        在消息文本中找到直接提及（在开头提及）
+        并返回提到的用户ID。 如果没有直接提及，则返回None
     """
     matches = re.search(MENTION_REGEX, message_text)
-    # the first group contains the username, the second group contains the remaining message
+    # 第一组包含用户名，第二组包含剩余消息 这里我删除了第二组以获取全部数据。
     return (matches.group(1).strip()) if matches else (None)
 
 def handle_command(command, channel):
     """
         Executes bot command if the command is known
     """
-    # Default response is help text for the user
+    # 默认回复
     default_response = "啊~  没有找到相应的指令呢 ⊂((・x・))⊃ " 
 	
-    # Finds and executes the given command, filling in response
+    # 查找并执行给定的命令，填写响应
     response = None
-    # This is where you start to implement more commands!
-    #这里开始
+    # 这里是填写更多命令的地方
     if command.startswith(EXANPLE_COMMAND): # 这一块就是命令执行部分。
         response = """是的，是的。"""
-    #这里结束 
-    # Sends the response back to the channel
+    # 将响应发送回通道
     slack_client.api_call(
         "chat.postMessage",
         channel=channel,
@@ -54,7 +52,7 @@ def handle_command(command, channel):
 if __name__ == "__main__":
     if slack_client.rtm_connect(with_team_state=False):
         print("Starter Bot connected and running!")
-        # Read bot's user ID by calling Web API method `auth.test`
+        # 通过调用Web API方法`auth.test`来读取bot的用户ID
         while True:
             command, channel = parse_bot_commands(slack_client.rtm_read())
             if command:
